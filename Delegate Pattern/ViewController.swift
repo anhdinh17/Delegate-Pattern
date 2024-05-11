e9e7387d10e4baeed4955d85f30c786c6ebfe4f8
Anh Dinh <anhdinh17@gmail.com>
2024-05-11 12:46:33 -0500

    Initial Commit

---
+++ Delegate Pattern/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate Pattern
+//
+//  Created by Anh Dinh on 5/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
